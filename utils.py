daf27afb13962e348c63021bfeffa2b5caa93346
GabHoo <gabriel.hoogerwerf@gmail.com>
2023-03-29 22:18:35 +0200

    sss

--- utils.py
+++ utils.py
@@ -5,10 +5,14 @@ def get_identified_verbs(pred):
     verbs=[x['verb'] for x in pred['verbs']]
     return verbs
 
-def evaluate_verbIdentification_DIR(predictor,data):
-    """This function return the failure rates percentage on a test set.
+def evaluate_PI_Polysem_DIR(predictor,data):
+    """
+    This function return the failure rates percentage on a test set.
     :predictor is predictor object
     :data is dictionary in the shape {'verb':[s1,s2]} 
+    WHERE s1 contains the wordd as a noun/adjective, S2 as verb
+
+    returns failure rate
     """
     correct=0
     for v in data:
@@ -20,7 +24,7 @@ def evaluate_verbIdentification_DIR(predictor,data):
         v1=get_identified_verbs(pred1)
         v2=get_identified_verbs(pred2)
 
-        if v not in v1 and v in v2:
+        if v not in v1 and v in v2: #IF verb is not found in s1 but is found in 2 is correct!
             correct+=1
             continue
         else:
@@ -30,39 +34,71 @@ def evaluate_verbIdentification_DIR(predictor,data):
             print("\n")
     return (len(data)-correct)/len(data)*100
 
-
-def evaluate_PI_INV_contractions(predictor,data):               
+def evaluate_PI_Contractions_INV(predictor,data):             
     """
     This function returns a failure rate (percentage) that represents how many coupled sentnece 
-    presents inconcitency in their tags of a contracted predicate. The idea is that contracting a verb should not cause any changes! 
-    Matching is done as following: tags need to be exactly the same thus the perturbation does not have to introduce
-    any more (or less) token or change the arguments. This checks only fits very few purposes.
+    presents inconcitency in their tags of a contracted predicate. The idea is that a predictor should be able to identify a verb even if varieted.
+    Matching is TAG BASED: The models must have predicted a verb in the given position (inflected verb), if not is a failure.
+
     :predictor
-    :data is a list of lists where the nested lists are couple of sentence to be compared
+    :data is a list of tupleslists where the nested lists are couple of sentence to be compared
+
+    returns a failure rate
     """
+
+    data=[(i,v[0],v[1]) for i,v in data.items()] #NECESSARY CONVERSION FROM FILE, ugly i know
+    
     fail=0
+    
     for s in data:
-        s1,s2=s
+        s1,s2,i=s
         pred1=predictor.predict(s1)
         pred2=predictor.predict(s2)
 
         v1=get_identified_verbs(pred1)
         v2=get_identified_verbs(pred2)
-        
+
         if len(v1)!=len(v2):
             print(f"Failure in {s1} | {s2}")
             print(f"Different number of verbs found: {v1=} against {v2=}")
             fail+=1
             continue
-        
+
+                
         for v1,v2 in zip(pred1['verbs'],pred2['verbs']):
             if v1['verb']!=v2['verb']: #we only compare the verbs that are not matching exactly (aka the contracted vs original)
-                if v1['tags']!=v2['tags']:
+                if v1['tags'][i]!='B-V' or v2['tags'][i]!='B-V': #And if one of them is not found to be a verb, test fails
                     fail+=1
                     print(f"Failure in {s1} | {s2}")
                     print(f"The alteration of verb {v1['verb']}/{v2['verb']} lead to a different labeling:\n")
                     print(v1)
                     print(v2)
-                    break
-            
+                    break   
+    
     return fail/len(data)*100
+
+
+def evaluate_PI_inflections_MFT(predictor,data):
+    """
+    This functions tests MFT for inflected verb forms. Models needs to be able to detect them as verbs, if not, fail
+    :predictor
+    :data is a dict {verb:sentence}
+    
+    output is failure rate
+    """
+
+    fails=0
+    for v in data:
+        s=data[v] 
+        
+        pred=predictor.predict(s)
+
+        verbs=get_identified_verbs(pred)
+       
+
+        if v not in verbs: #IF verb is not found in s1 but is found in 2 is correct!
+            fails+=1
+            print(f"Failed for: {s} did not detect {v}\n")
+            continue
+     
+    return (fails)/len(data)*100
